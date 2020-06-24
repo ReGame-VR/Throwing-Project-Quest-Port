6714a5fea8a99c605a9e977ee752592ecba0efcd
clrj14 <33142639+clrj14@users.noreply.github.com>
2020-06-23 21:40:23 -0700

    Handedness by touching sphere added

---
+++ Regame Port/Assets/Scripts/Cody/HandActivationSetting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandActivationSetting : MonoBehaviour
+{
+    public GlobalControl globalControl;
+    public GameObject calibrationEnvironment;
+
+    /**
+     * Checking if player has put in there dominant hand.
+     * If they have, then we set their dominant hand
+     * in the global control script. 
+     */
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.parent.name == "CustomHandLeft")
+        {
+            globalControl.isRightHanded = false;
+        }
+
+        if (other.transform.parent.name == "CustomHandRight")
+        {
+            globalControl.isRightHanded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Turn on calibration scene when handedness has been set.
+        calibrationEnvironment.SetActive(true);
+
+        //Disable this gameobject since handedness has been chosen.
+        this.gameObject.SetActive(false);
+    }
+}
